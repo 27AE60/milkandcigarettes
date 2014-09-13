81c013b76f4eeb97798ba8a29c39746c9c701be8
Jaison Justus <jaison.justus@bookmyshow.com>
2014-09-13 23:24:08 +0530

    Basic prompt and code structure
    Cheers!! :beer:

--- large.js
+++ large.js
@@ -1,9 +1,30 @@
-var markdown = require('markdown').markdown,
-    yargs = require('yargs');
+#!/usr/bin/env node
 
-// TODO: get filename as arguments from terminal for publish.
-// TODO: read markdown file.
-// TODO: convert it to html.
-// TODO: embed into the page template.
-// TODO: create html file in static folder.
-// TODO: update the home page list.
+var markdown = require('markdown').markdown,
+    argv = require('yargs').argv;
+
+var Large = {
+  folder : {
+    markdown : [process.cwd(), 'post/'].join('/'),
+    html : ''
+  },
+
+  // TODO: read markdown file.
+  _readMarkdown : function(filename)  {
+    console.log(this.folder.markdown);
+    console.log(filename);
+  },
+
+  // TODO: convert it to html.
+  // TODO: embed into the page template.
+  // TODO: create html file in static folder.
+  // TODO: update the home page list.
+  // TODO: publish the article.
+  publish : function(filename)  {
+    this._readMarkdown(filename);
+  }
+};
+
+if(argv.p)  {
+  Large.publish(argv.p);
+}
