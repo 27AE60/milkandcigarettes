151cd33e9d93a2212ac939cea95cc50b030ce02a
Jaison Justus <jaison.justus@bookmyshow.com>
2014-09-13 23:09:06 +0530

    Kickstart Publish feature.

---
+++ large.js
@@ -0,0 +1,9 @@
+var markdown = require('markdown').markdown,
+    yargs = require('yargs');
+
+// TODO: get filename as arguments from terminal for publish.
+// TODO: read markdown file.
+// TODO: convert it to html.
+// TODO: embed into the page template.
+// TODO: create html file in static folder.
+// TODO: update the home page list.
